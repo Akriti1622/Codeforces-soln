c8e6609216225da92cdc21c9338ed272aea8c3d7
Akriti1622 <66354346+Akriti1622@users.noreply.github.com>
2021-11-07 17:36:54 +0530

    In search of an easy problem

---
+++ 1030A.cpp
@@ -0,0 +1,18 @@
+//1030A - In search of an easy problem
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int t;
+   cin>>t;
+   while(t>0) {
+       int a;
+       cin>>a;
+               if(a==1)
+                   cout<<"HARD";
+                   t--;
+                 }
+                 if(t==0)
+                 cout<<"EASY";
+                 return 0;
+               }
