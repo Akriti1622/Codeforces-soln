aa5a92443a5eda17662f30afbbe2c737ad24ec44
Akriti1622 <66354346+Akriti1622@users.noreply.github.com>
2022-01-08 12:37:56 +0530

    Word Capitalization

---
+++ 281A.cpp
@@ -0,0 +1,15 @@
+//281A - Word Capitalization
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    string word;
+    cin>>word;
+    if(word[0]>=97&&word[0]<=122) {
+        word[0]=word[0]-32;
+    }
+    cout<<word;
+   return 0;
+}
