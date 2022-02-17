c42ff7082ab2af42d7f99566a7b4dc2306424cea
Akriti1622 <66354346+Akriti1622@users.noreply.github.com>
2022-02-17 18:00:58 +0530

    Domino piling

---
+++ 50A.cpp
@@ -0,0 +1,13 @@
+//Domino piling - 50A
+#include <iostream>
+using namespace std;
+ 
+int main() 
+{
+    int m, n, squares, ans;
+    cin>>m>>n;
+    squares=m*n;
+    ans=squares/2;
+    cout<<ans;
+    return 0;
+}
